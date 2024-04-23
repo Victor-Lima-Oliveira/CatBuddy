83ddc7dcf1804872ccd7558190acb4f725d8ec07
PedroPog <victorlima.oliveira70@gmail.com>
2024-04-22 21:14:15 -0300

    Front
    Atualizações do design e upload de fotos

--- CatBuddy/obj/Debug/net8.0/CatBuddy.AssemblyInfo.cs
+++ CatBuddy/obj/Debug/net8.0/CatBuddy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatBuddy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d108681f18e3894b553987db079aca5f7841e2e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c05120736284cc75a757cfda53b85d82a7c9bcba")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatBuddy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatBuddy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
