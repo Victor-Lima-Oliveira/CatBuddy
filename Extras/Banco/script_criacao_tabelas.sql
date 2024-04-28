ec07e63d5a30dfe0ab704fac9e6226b7dffd68a2
Victor Lima de Oliveira <victorlima.oliveira70@gmail.com>
2024-04-28 14:46:48 -0300

    Atualizações no banco de dados, recomendado dropar a database e criar as tabelas novamente

--- Extras/Banco/script_criacao_tabelas.sql
+++ Extras/Banco/script_criacao_tabelas.sql
@@ -32,6 +32,18 @@ CREATE TABLE tbl_enderecoFornecedor (
     cod_fornecedor int,
    foreign key (cod_fornecedor) references tbl_fornecedor (cod_id_fornecedor),
    foreign key (cod_logradouro) references tbl_logradouro (cod_id_logradouro));
+   
+   create table tbl_informacoesNutricionais (
+cod_produto int, 
+TamanhoOuPorcao varchar(10),
+caloriaPorPorcao varchar(10),
+proteinas varchar(10),
+carboidratos varchar(10),
+vitaminas varchar(10),
+mineirais varchar(10),
+fibraDiétrica varchar(10),
+Colesterol varchar(10),
+Sodio varchar(10));
 
 CREATE TABLE tbl_produto (
     cod_id_produto int PRIMARY KEY auto_increment,
@@ -41,7 +53,6 @@ CREATE TABLE tbl_produto (
     cod_fornecedor int not null,
     idade varchar(13),
     sabor varchar(45),
-    informacoesNutricionais text,
     cor varchar(45),
     medidasAproximadas varchar(45),
     material varchar(45),
@@ -101,3 +112,25 @@ subtotal float not null,
 foreign key (cod_produto) references tbl_produto (cod_id_produto),
 foreign key (cod_pedido) references tbl_pedido (cod_id_pedido));
  
+-- Criada uma view do produto para facilitar as consultas
+create view vwProduto as
+SELECT 
+t1.cod_id_produto, 
+t1.descricao,
+ t1.qtdestoque,
+ t1.idade, 
+ t1.sabor, 
+ t1.cor,
+t1.medidasaproximadas, 
+t1.material,
+ t1.composicao, 
+ t1.preco, 
+ t1.imgpath,
+ t1.ds_nome,
+ t2.cod_id_fornecedor "codFornecedor",
+t2.nomeFantasia "fornecedor" ,
+t3.cod_id_categoria "codCategoria",
+t3.nomeCategoria "Categoria"
+FROM tbl_produto t1 
+INNER JOIN tbl_fornecedor t2 ON  t1.cod_fornecedor = t2.cod_id_fornecedor
+INNER JOIN tbl_categoria t3 ON t1.cod_categoria = t3.cod_id_categoria;
