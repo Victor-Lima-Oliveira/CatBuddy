507cff9f32167dceadc1823a38066418e42bcd9c
Victor Lima de Oliveira <victorlima.oliveira70@gmail.com>
2024-05-13 23:32:36 -0300

    Slides da página principal realizada com sucesso, tentativa de criar o carrossel com os dados das categorias, fracassado

--- CatBuddy/Controllers/HomeController.cs
+++ CatBuddy/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using CatBuddy.httpContext;
 using CatBuddy.Models;
+using CatBuddy.Repository.Contract;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,10 +8,17 @@ namespace CatBuddy.Controllers
 {
     public class HomeController : Controller
     {
+        private IProdutoRepository _produtoRepository;
+        protected List<Categoria> listCategoria = new List<Categoria>();
+
+        public HomeController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
 
         public IActionResult Index()
-        {
-            return View();
+        { 
+            return View(_produtoRepository.RetornaCategorias());
         }
 
     }
